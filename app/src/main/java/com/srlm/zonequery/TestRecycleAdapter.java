2c1de664ec31dabe3ec72929e015555e9f6f4d11
srlm <dongyuqi_good@163.com>
2022-09-03 20:49:04 +0800

    update .gitignore

---
+++ app/src/main/java/com/srlm/zonequery/TestRecycleAdapter.java
@@ -0,0 +1,29 @@
+package com.srlm.zonequery;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * @Description
+ * @Time
+ */
+public class TestRecycleAdapter extends RecyclerView.Adapter{
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
